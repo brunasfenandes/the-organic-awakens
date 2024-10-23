77a68598f04c4cec3c963f25d5a3cbc4a0f3d4b0
brunasfenandes <bruna.fenandes@outlook.com>
2024-10-23 17:55:58 -0400

    Added proper link to about page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -58,7 +58,7 @@ export default function Header() {
 
           <div className='header__menu'>
             <ul className='menu__list'>
-              <Link to="/" className='menu__list--item'>
+              <Link to="/about" className='menu__list--item'>
                 About
               </Link>
               <Link to="/flashcards" className='menu__list--item'>
